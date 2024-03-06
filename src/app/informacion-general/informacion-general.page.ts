cb3fbd2161cb419902146153a172916746e2d922
Juan Manuel <jherram0603@g.educaand.es>
2024-03-06 12:39:39 +0100

    arregla el mapa de la segunda pagina

--- src/app/informacion-general/informacion-general.page.ts
+++ src/app/informacion-general/informacion-general.page.ts
@@ -8,32 +8,27 @@ import * as L from 'leaflet';
 })
 export class InformacionGeneralPage implements OnInit {
   
-  map: any;
-  marker:any;
+  map: L.Map;
+  marker: L.Marker;
 
   constructor() {
-    this.map = {}  as L.Map;
-    this.marker = {}  as L.Marker;
+    this.map = {} as L.Map;
+    this.marker = {} as L.Marker;
    }
 
   ngOnInit() {
   }
-  ionViewDidEnter(){
+
+  ionViewDidEnter() {
     this.loadMap();
   }
 
   loadMap() {
-    let latitud = 36.6797047;
-    let longitud = -5.4470656;
+    let latitud = 36.66310879451221;
+    let longitud = -5.451101782693781;
     let zoom = 17;
     this.map = L.map("mapId").setView([latitud, longitud], zoom);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-        .addTo(this.map);
-
-        const markPoint = L.marker([latitud, longitud]);
-        markPoint.bindPopup('<p>Ntra. Sra. de los Remedios - Ubrique.</p>');
-        this.map.addLayer(markPoint);
-        this.map.dragging.enable();
-      }
-      
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+    this.marker = L.marker([latitud, longitud]).addTo(this.map);
+  }
 }
